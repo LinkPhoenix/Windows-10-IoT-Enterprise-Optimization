189721142f2bc3db2b1455d356dbc5160ee8d3b3
LinkPhoenix <33618968+LinkPhoenix@users.noreply.github.com>
2018-06-27 17:55:43 +0200

    Update Edit registered owner.bat

--- Edit registered owner.bat
+++ Edit registered owner.bat
@@ -3,5 +3,5 @@
 :: URL Source : https://www.howtogeek.com/howto/windows-vista/quick-tip-change-the-registered-owner-in-windows/
 :: Comment : Use Windows+R to see the changes
 ::
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOwner" /t "REG_DWORD" /d "Owner" /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t "REG_DWORD" /d "Organization owner" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOwner" /t "REG_SZ" /d "Owner" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t "REG_SZ" /d "Organization owner" /f
