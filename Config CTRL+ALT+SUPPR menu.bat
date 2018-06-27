48346122f77eae382cce1a4bda1863bb2c802b3d
LinkPhoenix <33618968+LinkPhoenix@users.noreply.github.com>
2018-06-27 18:11:21 +0200

    Fix remove "Switch user" from the ALT+CTRL+DEL

--- Config CTRL+ALT+SUPPR menu.bat
+++ Config CTRL+ALT+SUPPR menu.bat
@@ -7,5 +7,5 @@ REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "N
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableChangePassword" /t "REG_DWORD" /d "1" /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableLockWorkstation" /t "REG_DWORD" /d "1"/f
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "HideFastUserSwitching" /t "REG_DWORD" /d "1" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "HideFastUserSwitching" /t "REG_DWORD" /d "1" /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoLogoff" /t "REG_DWORD" /d "1" /f
