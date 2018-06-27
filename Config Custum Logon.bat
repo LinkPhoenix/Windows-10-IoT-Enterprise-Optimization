4ae56e33794fe4f4e8c2c436cb8caf0a1686c033
LinkPhoenix <33618968+LinkPhoenix@users.noreply.github.com>
2018-06-27 18:27:35 +0200

    Fix custum logon

--- Config Custum Logon.bat
+++ Config Custum Logon.bat
@@ -6,8 +6,8 @@
 Dism -Online /Enable-Feature /FeatureName:Client-DeviceLockdown /NoRestart
 Dism /Online /Enable-Feature /FeatureName:Client-EmbeddedLogon /NoRestart
 :: Hides items that appear on the connection screen
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogo" /v "BrandingNeutral" /t "REG_DWORD" /d "17" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogon" /v "BrandingNeutral" /t "REG_DWORD" /d "17" /f
 :: Hides the whole process of auto-connection
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogo" /v "HideAutoLogonUI" /t "REG_DWORD" /d "1" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogon" /v "HideAutoLogonUI" /t "REG_DWORD" /d "1" /f
 :: Disable the "First Logon Animation"
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogo" /v "HideFirstLogonAnimation" /t "REG_DWORD" /d "1" /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows Embedded\EmbeddedLogon" /v "HideFirstLogonAnimation" /t "REG_DWORD" /d "1" /f
